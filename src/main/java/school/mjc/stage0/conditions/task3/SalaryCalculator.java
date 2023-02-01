b5f22f66bcfa521f5180d973a05571f6f64abeea
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-01 14:29:10 +0400

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,11 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-            double calculatedSalary = 0;
-            if(salary<=10000) calculatedSalary=salary-(salary/100)*15;
-            else if(salary>10000 && salary<=20000)calculatedSalary=salary-(salary/100)*17;
-            else if( salary>20000)calculatedSalary=salary-(salary/100)*20;
-            else if(salary<0) System.out.println("wrong input!");
-            if(calculatedSalary!=0) System.out.println(calculatedSalary);
+             double calculatedSalary = 0;
+             if(salary<=10000 && salary>0){ calculatedSalary=salary-(salary/100)*15;}
+             else if(salary>10000 && salary<=20000){calculatedSalary=salary-(salary/100)*18;}
+             else if( salary>20000){calculatedSalary=salary-(salary/100)*20;}
+             else if(salary<=0){ System.out.println("wrong input!");}
+             if(calculatedSalary>0) System.out.println(calculatedSalary);
     }
 }
